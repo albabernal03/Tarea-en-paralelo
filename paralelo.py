0e8d105c4091a9597ce777cb3a2b62da3b50ea36
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2023-02-20 16:00:53 +0100

    creamos la funion que se encarga del scraping

--- paralelo.py
+++ paralelo.py
@@ -3,3 +3,15 @@
 #En primer lugar, creamos una lista:
 
 url=["a.com", "b.com", "c.com", "d.com"]
+
+import random  #Importamos la librería random para generar números aleatorios
+from time import sleep  #Importamos la librería sleep para esperar un tiempo determinado
+
+#Esta función se encarga de simular el proceso de scraping de una página web. Simplemente imprime un mensaje y espera un tiempo aleatorio entre 0 y 1 segundos.
+def scrape(url):
+    print("starting", url)
+    duration = round(random.random(), 3)
+    sleep(duration)
+    print("finished", url, "time taken:", duration, "seconds")
+    return url, duration
+
